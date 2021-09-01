261b1edc3bbe160a5a2ba7b87b01af2e3444049e
Seokho-Ham <seokho9522@gmail.com>
2021-09-01 12:00:56 +0900

    Login - Login Provider 작성
    - 새로고침 시 전역에 저장되어있는 로그인 상태가 날아가버리는 문제를
    해결하기 위해서 Provider를 작성
    - LoginProvider Component를 만들어서 렌더링 해야할 컴포넌트를 감싸는
    방식으로(미들웨어 개념으로 사용)

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import { ThemeProvider } from 'styled-components';
 import { myTheme } from './styles/theme';
 import Header from './components/common/Header';
 import { GlobalStyle } from './styles/global-style';
+import LoginProvider from './components/login/LoginProvider';
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={myTheme}>
       <GlobalStyle />
 
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={LoginPage} />
-          <Route path='/login' component={LoginPage} />
-          <Route path='/boards' component={MainPage} />
-          <Route path='/board/:id/cards' component={BoardPage} />
-        </Switch>
-      </BrowserRouter>
+      <LoginProvider>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={LoginPage} />
+            <Route path='/login' component={LoginPage} />
+            <Route path='/boards' component={MainPage} />
+            <Route path='/board/:id/cards' component={BoardPage} />
+          </Switch>
+        </BrowserRouter>
+      </LoginProvider>
     </ThemeProvider>
   );
 };
